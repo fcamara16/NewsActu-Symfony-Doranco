e5692459fa837f9f139acdf9cee91c9f025f834c
fcamara16 <fatoumatacamara1611@gmail.com>
2022-03-23 16:59:51 +0100

    add  user profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends AbstractController
 {
+
+    /**
+     * @Route("/profile", name="show_profile", methods={"GET"})
+     */
+    public function showProfile(): Response
+    {
+        return $this->render('profile/show_profile.html.twig');
+    }
 }
